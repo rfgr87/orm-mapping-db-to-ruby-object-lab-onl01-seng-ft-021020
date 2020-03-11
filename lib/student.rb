5d968fc4508e4226060b9f29ff908ad8ba99991d
Roy F Guzman Rodriguez <royguzrod@gmail.com>
2020-03-11 18:32:35 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -34,13 +34,13 @@ class Student
     end
   end
   
-  def self.first_X_students_in_grade_10
+  def self.first_X_students_in_grade_10(x)
     sql = <<-SQL
       SELECT *
-      FROM students WHERE grade = 10 ORDER BY students.id ASC LIMIT 1
+      FROM students WHERE grade = 10 ORDER BY students.id ASC LIMIT >
     SQL
     
-    DB[:conn].execute(sql).collect do |row|
+    DB[:conn].execute(sql, x).collect do |row|
       self.new_from_db(row)
     end.first 
   end
